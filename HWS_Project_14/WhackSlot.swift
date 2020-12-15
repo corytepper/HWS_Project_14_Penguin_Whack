13fc68d1dc8eb99f8aef36a45c13368287058f65
Cory Tepper <corytepper@Corys-MacBook-Pro.local>
2020-12-15 12:50:13 -0800

    added all game logic and touch methods - basic play is go

--- HWS_Project_14/WhackSlot.swift
+++ HWS_Project_14/WhackSlot.swift
@@ -13,6 +13,10 @@ class WhackSlot: SKNode {
     
     var charNode: SKSpriteNode!
     
+    var isVisible = false
+    var isHit = false
+    
+    
     func configure(at position: CGPoint) {
         self.position = position
         
@@ -33,5 +37,49 @@ class WhackSlot: SKNode {
         addChild(cropNode)
         
     }
+    
+    func show(hideTime: Double) {
+        if isVisible { return }
+        
+        charNode.xScale = 1
+        charNode.yScale = 1
+        
+        
+        
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        
+        isVisible = true
+        isHit = false
+        
+        if Int.random(in: 0...2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) {
+            [weak self] in
+            self?.hide()
+        }
+    }
+    
+    func hide() {
+        if !isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
+    }
 
+    
+    func hit() {
+        isHit = true
+        
+        let delay = SKAction.wait(forDuration: 0.25)
+        let hide = SKAction.moveBy(x: 0, y: -80, duration: 0.5)
+        let notVisible = SKAction.run { [weak self] in self?.isVisible = false }
+        let sequence = SKAction.sequence([delay, hide, notVisible])
+        charNode.run(sequence)
+    }
 }
